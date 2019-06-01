0c367b01d00f642d2e40c9f8c2a02e61ecec62b3
daojiwu <2582557610@qq.com>
2019-06-01 12:15:19 +0800

    code1的冲突等等

--- src/com/newroad/test/Test.java
+++ src/com/newroad/test/Test.java
@@ -3,7 +3,7 @@ package com.newroad.test;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Test");
-		System.out.println("手打");
+		System.out.println("手打等冲突");
 	}
 
 }
